8a68b8f576a034bf42666bcfc1ba52428df34d97
Nate Burley <natemburl@gmail.com>
2023-12-02 00:01:48 -0800

    Adjusted Shakespeare dataset class to build chunks of size tokenizer.max_length to fix warning

--- utils/datasets.py
+++ utils/datasets.py
@@ -4,15 +4,17 @@ from torch.utils.data import Dataset
 
 class ShakespeareDataset(Dataset):
     def __init__(self, csv_file, chunk_length, tokenizer):
-        # Read and concatenate text as before
         self.df = pd.read_csv(csv_file)
-        self.all_text = ' '.join(self.df['PlayerLine'].tolist())
+        all_text = ' '.join(self.df['PlayerLine'].tolist())
 
-        # Tokenize the text
-        self.tokenizer = tokenizer
-        self.tokens = self.tokenizer.encode(self.all_text)
+        # Tokenize the text in smaller chunks
+        max_length = tokenizer.model_max_length  # 1024 for models like GPT-2
+        self.tokens = []
 
-        # Set chunk length (2k)
+        for i in range(0, len(all_text), max_length):
+            chunk = all_text[i:i+max_length]
+            self.tokens.extend(tokenizer.encode(chunk))
+
         self.chunk_length = chunk_length
 
     def __len__(self):
@@ -20,6 +22,6 @@ class ShakespeareDataset(Dataset):
 
     def __getitem__(self, index):
         start = index * self.chunk_length
-        end = start + self.chunk_length
+        end = min(start + self.chunk_length, len(self.tokens))
         return torch.tensor(self.tokens[start:end])
 
