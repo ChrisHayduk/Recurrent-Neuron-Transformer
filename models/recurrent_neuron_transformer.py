5154d648d2077220d22f15898f712da2e7c255ab
Chris Hayduk <chayduk@fordham.edu>
2023-12-03 19:20:21 -0500

    Update transformer

--- models/recurrent_neuron_transformer.py
+++ models/recurrent_neuron_transformer.py
@@ -81,20 +81,11 @@ class RecurrentNeuronTransformer(nn.Module):
         :returns: the model outputs. Should be scores of shape (N,T,output_size).
         """
 
-        #############################################################################
-        # TODO:
-        # Deliverable 5: Implement the full Transformer stack for the forward pass. #
-        # You will need to use all of the methods you have previously defined above.#
-        # You should only be calling TransformerTranslator class methods here.      #
-        #############################################################################
         outputs = self.embed(inputs)
         outputs = self.multi_head_attention(outputs)
         outputs = self.feedforward_layer(outputs)
         outputs = self.final_layer(outputs)
         
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
         return outputs
     
     
@@ -103,20 +94,12 @@ class RecurrentNeuronTransformer(nn.Module):
         :param inputs: intTensor of shape (N,T)
         :returns embeddings: floatTensor of shape (N,T,H)
         """
-        #############################################################################
-        # TODO:
-        # Deliverable 1: Implement the embedding lookup.                            #
-        # Note: word_to_ix has keys from 0 to self.vocab_size - 1                   #
-        # This will take a few lines.                                               #
-        #############################################################################
       
         embeddings = self.embeddingL(inputs)
         pos_indices = torch.arange(0, inputs.size(1), device=self.device).unsqueeze(0).repeat(inputs.size(0), 1)
         pos_embeddings = self.posembeddingL(pos_indices)
         embeddings  = embeddings + pos_embeddings
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
+
         return embeddings
         
     def multi_head_attention(self, inputs):
@@ -128,12 +111,6 @@ class RecurrentNeuronTransformer(nn.Module):
         This is a simplified implementation.
         """
         
-        
-        #############################################################################
-        # TODO:
-        # Deliverable 2: Implement multi-head self-attention followed by add + norm.#
-        # Use the provided 'Deliverable 2' layers initialized in the constructor.   #
-        #############################################################################
         # Head #1
         k1 = self.k1(inputs)
         v1 = self.v1(inputs)
@@ -162,9 +139,6 @@ class RecurrentNeuronTransformer(nn.Module):
 
         outputs = self.norm_mh(proj + inputs)
         
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
         return outputs
     
     
@@ -174,18 +148,8 @@ class RecurrentNeuronTransformer(nn.Module):
         :returns outputs: float32 Tensor of shape (N,T,H)
         """
         
-        #############################################################################
-        # TODO:
-        # Deliverable 3: Implement the feedforward layer followed by add + norm.    #
-        # Use a ReLU activation and apply the linear layers in the order you        #
-        # initialized them.                                                         #
-        # This should not take more than 3-5 lines of code.                         #
-        #############################################################################
         outputs = self.norm_linear(inputs + self.linear2(torch.relu(self.linear1(inputs))))
         
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
         return outputs
         
     
@@ -195,199 +159,6 @@ class RecurrentNeuronTransformer(nn.Module):
         :returns outputs: float32 Tensor of shape (N,T,V)
         """
         
-        #############################################################################
-        # TODO:
-        # Deliverable 4: Implement the final layer for the Transformer Translator.  #
-        # This should only take about 1 line of code.                               #
-        #############################################################################
         outputs = self.linear_output(inputs)
                 
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
         return outputs
-        
-
-class FullTransformerTranslator(nn.Module):
-
-    def __init__(self, input_size, output_size, device, hidden_dim=128, num_heads=2,
-                 dim_feedforward=2048, num_layers_enc=2, num_layers_dec=2, dropout=0.2, max_length=43, ignore_index=1):
-        super(FullTransformerTranslator, self).__init__()
-
-        self.num_heads = num_heads
-        self.word_embedding_dim = hidden_dim
-        self.hidden_dim = hidden_dim
-        self.dim_feedforward = dim_feedforward
-        self.max_length = max_length
-        self.input_size = input_size
-        self.output_size = output_size
-        self.device = device
-        self.pad_idx=ignore_index
-
-        seed_torch(0)
-
-        ##############################################################################
-        # TODO:
-        # Deliverable 1: Initialize what you need for the Transformer Layer          #
-        # You should use nn.Transformer                                              #
-        ##############################################################################
-
-        self.transformer = nn.Transformer(d_model=self.hidden_dim, nhead=self.num_heads, num_encoder_layers=num_layers_enc, num_decoder_layers=num_layers_dec, dim_feedforward=self.dim_feedforward, dropout=dropout, batch_first=True, device=self.device)
-
-        ##############################################################################
-        # TODO:
-        # Deliverable 2: Initialize what you need for the embedding lookup.          #
-        # You will need to use the max_length parameter above.                       #
-        # Initialize embeddings in order shown below.                                #
-        # Don’t worry about sine/cosine encodings- use positional encodings.         #
-        ##############################################################################
-        self.srcembeddingL = nn.Embedding(self.input_size, self.word_embedding_dim)     
-        self.tgtembeddingL = nn.Embedding(self.input_size, self.word_embedding_dim) 
-        
-        
-         
-        self.srcposembeddingL = nn.Embedding(self.max_length, self.word_embedding_dim)     
-        self.tgtposembeddingL = nn.Embedding(self.max_length, self.word_embedding_dim)
-
-        ##############################################################################
-        # TODO:
-        # Deliverable 3: Initialize what you need for the final layer.               #
-        ##############################################################################
-        self.final_layer = nn.Linear(self.hidden_dim, self.output_size)
-
-
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
-
-    def forward(self, src, tgt):
-        """
-         This function computes the full Transformer forward pass used during training.
-         Put together all of the layers you've developed in the correct order.
-
-         :param src: a PyTorch tensor of shape (N,T) these are tokenized input sentences
-                tgt: a PyTorch tensor of shape (N,T) these are tokenized translations
-         :returns: the model outputs. Should be scores of shape (N,T,output_size).
-         """
-        #############################################################################
-        # TODO:
-        # Deliverable 4: Implement the full Transformer stack for the forward pass. #
-        #############################################################################
-
-        # shift tgt to right, add one <sos> to the beginning and shift the other tokens to right
-        tgt = self.add_start_token(tgt)
-
-        # embed src and tgt for processing by transformer
-        src_embed = self.srcembeddingL(src) 
-        tgt_embed = self.tgtembeddingL(tgt)
-
-        src_positions = torch.arange(0, src.size(1)).unsqueeze(0).repeat(src.size(0),1).to(self.device)
-        tgt_positions = torch.arange(0, tgt.size(1)).unsqueeze(0).repeat(tgt.size(0),1).to(self.device)
-        
-        src_embed += self.srcposembeddingL(src_positions)
-        tgt_embed += self.tgtposembeddingL(tgt_positions)
-
-        # create target mask and target key padding mask for decoder
-        tgt_mask = (self.transformer.generate_square_subsequent_mask(tgt.shape[1], device=self.device)).bool()
-        tgt_key_padding_mask = (tgt == self.pad_idx)
-        
-        # invoke transformer to generate output
-        outputs = self.final_layer(self.transformer.forward(src_embed, tgt_embed, tgt_mask = tgt_mask, tgt_key_padding_mask = tgt_key_padding_mask))
-
-        ##############################################################################
-        #                               END OF YOUR CODE                             #
-        ##############################################################################
-        return outputs
-
-    def generate_translation(self, src):
-        """
-         This function generates the output of the transformer taking src as its input
-         it is assumed that the model is trained. The output would be the translation
-         of the input
-
-         :param src: a PyTorch tensor of shape (N,T)
-
-         :returns: the model outputs. Should be scores of shape (N,T,output_size).
-         """
-        #############################################################################
-        # TODO:
-        # Deliverable 5: You will be calling the transformer forward function to    #
-        # generate the translation for the input.                                   #
-        #############################################################################
-        # Initialize the target tensor with the start token
-        tgt = torch.zeros((src.shape[0], src.shape[1]), dtype=torch.long) + self.pad_idx
-        print(tgt.shape)
-
-        outputs = torch.zeros((src.shape[0], src.shape[1], self.output_size))
-
-        tgt[:, 0] = src[:, 0]
-
-        for i in range(src.shape[1]):
-            # Run the forward pass
-            output = self.forward(src, tgt)
-
-            # Get the next token from the output
-            next_token = output.argmax(dim=-1)[:, i]
-
-            if i+1 < tgt.shape[1]:
-                # Append the next token to the tgt sequence
-                tgt[:, i+1] = next_token
-
-            outputs[:, i, :] = output[:,i,:]
-
-
-        return outputs
-
-    def add_start_token(self, batch_sequences, start_token=2):
-        """
-            add start_token to the beginning of batch_sequence and shift other tokens to the right
-            if batch_sequences starts with two consequtive <sos> tokens, return the original batch_sequence
-
-            example1:
-            batch_sequence = [[<sos>, 5,6,7]]
-            returns:
-                [[<sos>,<sos>, 5,6]]
-
-            example2:
-            batch_sequence = [[<sos>, <sos>, 5,6,7]]
-            returns:
-                [[<sos>, <sos>, 5,6,7]]
-        """
-        def has_consecutive_start_tokens(tensor, start_token):
-            """
-                return True if the tensor has two consecutive start tokens
-            """
-            consecutive_start_tokens = torch.tensor([start_token, start_token], dtype=tensor.dtype,
-                                                    device=tensor.device)
-
-            # Check if the first two tokens in each sequence are equal to consecutive start tokens
-            is_consecutive_start_tokens = torch.all(tensor[:, :2] == consecutive_start_tokens, dim=1)
-
-            # Return True if all sequences have two consecutive start tokens at the beginning
-            return torch.all(is_consecutive_start_tokens).item()
-
-        if has_consecutive_start_tokens(batch_sequences, start_token):
-            return batch_sequences
-
-        # Clone the input tensor to avoid modifying the original data
-        modified_sequences = batch_sequences.clone()
-
-        # Create a tensor with the start token and reshape it to match the shape of the input tensor
-        start_token_tensor = torch.tensor(start_token, dtype=modified_sequences.dtype, device=modified_sequences.device)
-        start_token_tensor = start_token_tensor.view(1, -1)
-
-        # Shift the words to the right
-        modified_sequences[:, 1:] = batch_sequences[:, :-1]
-
-        # Add the start token to the first word in each sequence
-        modified_sequences[:, 0] = start_token_tensor
-
-        return modified_sequences
-
-def seed_torch(seed=0):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
