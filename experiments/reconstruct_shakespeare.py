7a3195d94470c1621c308f195782ef657d5c1e44
Nate Burley <natemburl@gmail.com>
2023-12-04 21:02:51 -0800

    Implemented main training loop. Need to validate GPU memory error source before merging

--- experiments/reconstruct_shakespeare.py
+++ experiments/reconstruct_shakespeare.py
@@ -1,23 +1,74 @@
 # General imports
-import os
-import numpy as np
-from tqdm.notebook import tqdm
-import copy
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from transformers import GPT2Tokenizer
-
-# Torch imports
+import argparse
 import torch
-import torch.nn as nn
-from torchvision import datasets, transforms
-from torch.utils.data import TensorDataset, DataLoader
 
 # Imports for the tokenizer, the dataset, and the model
 from transformers import GPT2Tokenizer
 from utils.datasets import TextDataLoader
 from models.transformer_model import TransformerModel
+from utils.training import train_shakespeare_transformer
 
 # Set random seed for reproducibility
 torch.manual_seed(0)
+
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="VanillaTransformer", help="Name of model. Options are \
+                        VanillaTransformer, StatefulTransformer, NanoGPT, TransformerXL")
+    parser.add_argument("--data_path", type=str, default="data/shakespeare/tinyshakespeare.txt", help="Path to the data file")
+    parser.add_argument("--chunk_size", type=int, default=2048, help="Size of the vocabulary")
+    parser.add_argument("--max_seq_length", type=int, default=1024, help="Maximum sequence length")
+    parser.add_argument("--window_step_size", type=int, default=64, help="Size of the vocabulary")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
+    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs")
+    parser.add_argument("--lr", type=float, default=0.001, help="Learning rate")
+    parser.add_argument("--nhead", type=int, default=8, help="Number of attention heads")
+    parser.add_argument("--num_encoder_layers", type=int, default=6, help="Number of encoder layers")
+    parser.add_argument("--num_decoder_layers", type=int, default=6, help="Number of decoder layers")
+    parser.add_argument("--dim_feedforward", type=int, default=2048, help="Dimension of the feedforward network")
+    args = parser.parse_args()
+
+    # Define the device
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+    else:
+        device = torch.device('cpu')
+    print(f"Using device: {device}")
+
+    # Create the tokenizer
+    tokenizer = 'gpt2'
+    vocab_size = 50257  # NOTE: HARD CODED FOR GPT2
+
+    # Create the data loader
+    data_loader = TextDataLoader(file_path=args.data_path, seq_length=args.chunk_size, bpe_tokenizer=tokenizer, 
+                                 batch_size=args.batch_size, vocab_size=vocab_size, split_ratio=0.8)
+    train_loader, test_loader = data_loader.create_loaders()
+
+    # Confirm model name is valid
+    assert args.model_name in ['VanillaTransformer', 'StatefulTransformer', 'NanoGPT', 'TransformerXL'], \
+        f"Invalid model name {args.model_name}"
+    
+    # Create the model
+    if args.model_name == 'VanillaTransformer':
+        model = TransformerModel(vocab_size=vocab_size, max_seq_length=args.max_seq_length, d_model=512, nhead=args.nhead, 
+                                 num_encoder_layers=args.num_encoder_layers, num_decoder_layers=args.num_decoder_layers, 
+                                 dim_feedforward=args.dim_feedforward).to(device)
+    elif args.model_name == 'StatefulTransformer':
+        raise NotImplementedError
+    elif args.model_name == 'NanoGPT':
+        raise NotImplementedError
+    elif args.model_name == 'TransformerXL':
+        raise NotImplementedError
+    
+    # Create the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    # Train the model
+    print(f"Training {args.model_name} on {args.data_path} with chunk size {args.chunk_size} and context window size {args.max_seq_length}")
+    train_shakespeare_transformer(model=model, context_window=args.chunk_size, step_size=args.window_step_size, 
+                                  data_loader=train_loader, optimizer=optimizer, num_epochs=args.num_epochs, 
+                                  device=device, mask=False)
---
+++ utils/training.py
@@ -0,0 +1,92 @@
+# General imports
+import os
+import numpy as np
+from tqdm.notebook import tqdm
+import copy
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from transformers import GPT2Tokenizer
+
+# Torch imports
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data import TensorDataset, DataLoader
+
+# Imports for the tokenizer, the dataset, and the model
+from transformers import GPT2Tokenizer
+from utils.datasets import TextDataLoader
+from models.transformer_model import TransformerModel
+
+
+
+# TODO: Using a mask breaks the training process due to a shape error. Needs to be fixed
+def create_look_ahead_mask(size):
+    """
+    Creates a mask for the target sequence to prevent the model from cheating by looking ahead in the sequence.
+
+    Args:
+        size (int): The size of the target sequence
+    
+    Returns:
+        torch.Tensor: The mask
+    """
+    mask = torch.triu(torch.ones(size, size), diagonal=1)
+    return mask.masked_fill(mask == 1, float('-inf')).masked_fill(mask == 0, float(0.0))
+
+
+# TODO: SHOULD THIS BE IN A UTILS FILE?
+# TODO: CONFIRM THAT RECURRENT TRANSFORMER MODEL HANDLES AND DOES NOT RETURN THE HIDDEN STATE
+def train_shakespeare_transformer(model, context_window, step_size, data_loader, optimizer, num_epochs, 
+                                   device, mask=False):
+    """
+    Trains a transformer model to reproduce large chunks of Shakespeare's plays by sliding a context window along a 
+    larger piece of text. The model is trained to predict the next word in the sequence given the context window.
+
+    Args:
+        model (nn.Module): The model to train
+        context_window (int): The size of the context window
+        step_size (int): The number of words to slide the context window by
+        data_loader (DataLoader): The data loader to use for training
+        optimizer (nn.optim): The optimizer to use for training
+        num_epochs (int): The number of epochs to train for
+        device (torch.device): The device to use for training
+        mask (bool): Whether to use a mask for the target sequence or not
+    
+    Returns:
+        None
+    """
+
+    for epoch in range(num_epochs):
+        model.train()
+        epoch_loss = 0
+        for inputs, labels in tqdm(data_loader, desc=f'Training: Epoch {epoch+1}/{num_epochs}', unit='batch'):
+            # Move the data to the device
+            input_seq = inputs.to(device)
+            target_seq = labels.to(device)
+            
+            # Optionally create a mask for the target sequence
+            if mask == True:
+                target_seq_mask = create_look_ahead_mask(target_seq.size(1)).to(device)
+                target_seq_mask = target_seq_mask.unsqueeze(0)
+            else:
+                target_seq_mask = None
+
+            # Zero the gradients
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(input_seq, target_seq, tgt_mask=target_seq_mask)
+            outputs = outputs.view(-1, outputs.size(-1))
+            target_seq = target_seq.view(-1)
+
+            # Calculate loss and backpropagate
+            loss = nn.CrossEntropyLoss()(outputs, target_seq)
+            loss.backward()
+            optimizer.step()
+
+            # Logging the loss and update progress bar
+            epoch_loss += loss.item()
+
+        print(f"Epoch {epoch+1}/{num_epochs} completed. Loss: {epoch_loss/len(data_loader)}")
