7f08f5767c026ed10b833a2d46f90d5b7db97e4f
Chris Hayduk <chayduk@fordham.edu>
2023-12-06 11:52:24 -0500

    Updated recurrent neuron layer so that it doesn't save hidden state from previous sequence

--- models/recurrent_neuron_layer.py
+++ models/recurrent_neuron_layer.py
@@ -8,25 +8,21 @@ class Neurons(nn.Module):
 
         # Initialize matrix neuron parameters and number of neurons to create
         self.n_neurons = n_neurons
-        self.params = nn.Parameter(torch.rand(n_neurons, 3, 3) * 2 - 1).to(self.device)
-
-        # Initialize hidden state for batch processing
-        self.register_buffer('hidden', torch.zeros(1, n_neurons, 1).to(self.device))
-        
+        self.params = nn.Parameter(torch.rand(n_neurons, 3, 3) * 2 - 1).to(self.device)        
 
         self.to(self.device)
     
     def forward(self, inputs, hidden_state=None):
         if hidden_state is not None:
-            self.hidden_state = hidden_state.detach()
+            hidden_state = hidden_state.detach()
         else:
-            self.hidden_state = torch.zeros(1, self.n_neurons, 1).to(self.device)
+            hidden_state = torch.zeros(1, self.n_neurons, 1).to(self.device)
 
         batch_size = inputs.shape[0]
         seq_len = inputs.shape[1]
 
         # Expand hidden to match batch size and sequence length
-        hidden_batch = self.hidden.expand(batch_size, seq_len, self.n_neurons, 1).to(self.device)
+        hidden_batch = hidden_state.expand(batch_size, seq_len, self.n_neurons, 1).to(self.device)
         
         # Ensure inputs is 3D: (batch_size, seq_len, n_neurons)
         inputs = inputs.view(batch_size, seq_len, -1, 1)
@@ -43,10 +39,8 @@ class Neurons(nn.Module):
 
         # Update hidden state without in-place operation
         new_hidden = dot[:, :, :, -1].unsqueeze(3).detach()
-
-        self.hidden = new_hidden
         
-        return dot[:, :, :, 0], self.hidden
+        return dot[:, :, :, 0], new_hidden
 
 class RecurrentNeuronLayer(nn.Module):
     def __init__(self, input_size, output_size, device):
