af1fc91f66eb3101282fec357c0fe122d192daeb
Chris Hayduk <chayduk@fordham.edu>
2023-12-09 00:28:04 -0500

    Updated vanilla transformer

--- models/vanilla_transformer_model.py
+++ models/vanilla_transformer_model.py
@@ -16,7 +16,7 @@ class VanillaTransformerModel(nn.Module):
 
         # Transformer Decoder
         transformer_encoder = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward, batch_first=True)
-        self.transformer_decoder = nn.TransformerEncoder(transformer_encoder, num_layers=num_decoder_layers)
+        self.transformer_decoder = nn.TransformerEncoder(transformer_encoder, num_layers=num_decoder_layers, norm=nn.LayerNorm([max_seq_length, d_model]))
 
         # Output layer
         self.out = nn.Linear(d_model, vocab_size)
@@ -31,17 +31,16 @@ class VanillaTransformerModel(nn.Module):
         return num_params
 
     def forward(self, tgt, tgt_mask=None):
-        # Generate causal mask
-        tgt_seq_len = tgt.size(0)
-        tgt_mask = generate_square_subsequent_mask(tgt_seq_len).to(tgt.device)
+        if tgt_mask is None:
+            tgt_seq_len = tgt.size(1)  # Assuming batch_first=True, change size index to 1
+            tgt_mask = generate_square_subsequent_mask(tgt_seq_len).to(tgt.device)
         
         tgt = self.embedding(tgt) * math.sqrt(self.d_model)
         tgt = self.pos_encoder(tgt)
-
-        # Since it's a decoder-only model, no encoder and memory
-        output = self.transformer_decoder(tgt, mask=tgt_mask, is_causal=True)
+        output = self.transformer_decoder(tgt, mask=tgt_mask)
         output = self.out(output)
         return output
+
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
